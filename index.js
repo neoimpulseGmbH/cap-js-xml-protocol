0e6f37d4129145f2fb47efe7b3ef70bffbb916e7
Patrick Weppelmann <patrick.weppelmann+KBGroup@neoimpulse.de>
2025-01-28 11:08:30 +0100

    fix: finalized xml protocol

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const cds = require('@sap/cds')
-const DEBUG = cds.debug('adapters')
-const XMLAdapter = require('./lib/XMLAdapter')
+const XMLAdapter = require('@neoimpulse/cap-js-xml-protocol/lib/XMLAdapter')
 
-let services
-const collectServicesAndMountAdapter = (srv, options) => {
-  if (!services) {
-    services = {}
-    cds.on('served', () => {
-      options.services = services
-      cds.app.use (options.path, cds.middlewares.before, XMLAdapter(options), cds.middlewares.after)
-      DEBUG?.('app.use(', options.path, ', ... )')
-    })
-  }
-  services[srv.name] = srv
-}
-
-module.exports = collectServicesAndMountAdapter
+module.exports = XMLAdapter
